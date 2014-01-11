d1c0a3174d414e5eb9b5c8f9fc307ad6afd6808d
Henry Ching <hching@yahoo-inc.com>
2014-01-10 23:53:34 -0800

    Initial Commit

---
+++ rottentomatoes/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  rottentomatoes
+//
+//  Created by Henry Ching on 1/10/14.
+//  Copyright (c) 2014 YahooHenry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
